345147e33e0f35dc50c4e9c89e59a0ea0b4b87d5
max <macastel00@gmail.com>
2021-02-01 19:58:11 -0600

    Added comments for readability Re-named some subroutines reset all meta - parameters
    Teams now tmID and each team has an ID, regaurdless of if they renamed or moved.
    
    This is fixed now

--- data_base.py
+++ data_base.py
@@ -2,35 +2,205 @@ import sqlite3
 from pandas import DataFrame
 
 class DataBaseManager:
-    def __init__(self, df=None, dn='PlayerData.db'):
-        self.df = df
-        # connect to the PlayerData.db database
-        self.conn = sqlite3.connect(dn)
 
-    def create_player_database(self):
-        # init an empty string to hold the column names to be used in the sql statement
-        column_names = ""
+  def __init__(self, df=None, dn='PlayerData.db'):
+    self.df = df
+    self.dn = dn
+    # connect to the PlayerData.db database
+    self.conn = sqlite3.connect(dn)
 
-        #  build the the string based on the names of the columns in the dataframe
-        for i, header in enumerate(self.df.columns.values.tolist()):
-            header = header.strip('#')
-            column_names = column_names + ", {} text".format(header)
+  def createDB(self):
+    c = self.conn.cursor()
 
-        # need to send command to the sql database
-        c = self.conn.cursor()
-        # string that will be the query statement to be executed on the database using the column name string.
-        # Table is created if it does not exists
-        qry = 'CREATE TABLE IF NOT EXISTS PLAYERS(id integer PRIMARY KEY {})'.format(column_names)
-        # print(qry)
-        # run the query statement
-        c.execute(qry)
-        # commit the changes to the database
-        self.conn.commit()
-        # transfer the dataframe data to a sql database
-        self.df.to_sql('PLAYERS', self.conn, if_exists='replace')
+    c.execute('''CREATE TABLE "Fantasy" (
+      "ftid"	INTEGER,
+      "year"	INTEGER,
+      "age"	INTEGER,
+      "G"	INTEGER,
+      "FantPos"	INTEGER,
+      "FantPt"	INTEGER,
+      "VBD"	INTEGER,
+      "PosRank"	INTEGER,
+      "OvRank"	INTEGER,
+      PRIMARY KEY("ftid")
+      );''')
 
+    c.execute('''CREATE TABLE "Rushing and Receiving" (
+        "ruretid"	INTEGER,
+        "year"		INTEGER,
+        "age"		INTEGER,
+        "TmID"		INTEGER,
+        "Pos"		VARCHAR(2),
+        "No."	INTEGER,
+        "G"		INTEGER,
+        "GS"		INTEGER,
+        "Rush"		INTEGER,
+        "RUSHYds"	INTEGER,
+        "RUSHTD"	INTEGER,
+        "RUSH1D"	INTEGER,
+        "RUSHLng"	INTEGER,
+        "RushY/A"	Real,
+        "RUSHY/G"	Real,
+        "RUSHA/G"	Real,
+        "TGT"		INTEGER,
+        "Rec"		INTEGER,
+        "RECYds"	INTEGER,
+        "Y/Rec"		REAL,
+        "RECTD"		INTEGER,
+        "REC1D"		INTEGER,
+        "RECLng"	INTEGER,
+        "Rec/G"		REAL,
+        "RECY/G"	REAL,
+        "CtchPct"		REAL,        
+        "Y/Tgt"		REAL,
+        "Touch"         INTEGER,
+        "Y/Tch"         REAL,
+        "YScm"          INTEGER,
+        "RRTD"          INTEGER,
+        "FMB"           INTEGER,
+        "AV"            INTEGER,
+        PRIMARY KEY("ruretid")
+        );''')
 
+    c.execute('''CREATE TABLE "Player" (
+            "pid"        INTEGER,
+            "Year"       INTEGER,
+            "Pos"        VARCHAR(2),
+            "Ht"         INTEGER,
+            "Wt"         INTEGER,
+            "40yd"       REAL,
+            "Bench"      INTEGER,
+            "Broad Jump" REAL,
+            "Shuttle"    REAL,
+            "3Cone"      REAL,
+            "Vertical"   REAL,
+            PRIMARY KEY("pid")
+            );''')
+    c.execute('''CREATE TABLE "Passing" (
+            "ptid"       INTEGER,
+            "Year"       INTEGER,
+            "Age"        INTEGER,
+            "TmID"        INTEGER,
+            "Pos"        VARCHAR(2),
+            "No."        INTEGER,
+            "G"          INTEGER,
+            "GS"         INTEGER,
+            "QBrec"      VARCHAR(20),
+            "Cmp"        INTEGER,
+            "Att"        INTEGER,
+            "Cmp%"       REAL,
+            "Yds"        INTEGER,
+            "TD"         INTEGER,
+            "TD%"        REAL,
+            "Int"        INTEGER,
+            "Int%"       REAL,
+            "1D"         INTEGER,
+            "Lng"        INTEGER,
+            "Y/A"        REAL,
+            "AY/A"       REAL,
+            "Y/C"        REAL,
+            "Y/G"        REAL,
+            "Rate"       REAL,
+            "QBR"        REAL,
+            "Sk"         INTEGER,
+            "SkYdsLost"  INTEGER,
+            "NY/A"       REAL,
+            "ANY/A"      REAL,
+            "Sk%"        REAL,
+            "4QC"        INTEGER,
+            "GWD"        INTEGER,
+            "AV"         INTEGER,
+            PRIMARY KEY("ptid")
+          );''')
+    c.execute('''CREATE TABLE "Adjusted Passing" (
+          "aptid"      INTEGER,
+          "Year"       INTEGER,
+          "Age"        INTEGER,
+          "TmID"        INTEGER,
+          "Pos"        VARCHAR(2),
+          "No."        INTEGER,
+          "G"          INTEGER,
+          "GS"         INTEGER,
+          "QBrec"      VARCHAR(20),	
+          "Att"        INTEGER,
+          "Y/A+"       REAL,
+          "NY/A+"      REAL,
+          "AY/A+"      REAL,
+          "ANY/A+"     REAL,
+          "Cmp%+"      REAL,
+          "TD%+"       REAL,
+          "Int%+"      REAL,
+          "Sack%+"     REAL,
+          "Rate+"      REAL,
+          PRIMARY KEY("aptid")
+          );''')
 
+    c.execute('''CREATE TABLE "Combined" (
+          "pid"        INTEGER,
+          "ttid"        INTEGER,  
+          "year"       INTEGER,
+          "ptid"       INTEGER,
+          "aptid"      INTEGER,
+          "ruretid"    INTEGER,
+          "ftid"       INTEGER,  
+          FOREIGN KEY("pid") REFERENCES COMBINE("PID"),
+          FOREIGN KEY("ttid") REFERENCES TEAM("TTID")
+          FOREIGN KEY("ptid") REFERENCES PASSING("PTID"),
+          FOREIGN KEY("aptid") REFERENCES ADJ_PASSING("APTID"),
+          FOREIGN KEY("ruretid") REFERENCES RUSHING_AND_RECEIVING("RURETID"),
+          FOREIGN KEY("ftid") REFERENCES FANTASY("FTID")
+        );''')
 
+    c.execute('''CREATE TABLE "Team Stats" (
+          "ttid"        INTEGER,
+          "TmID"         INTEGER,
+          "year"        INTEGER,
+          "Player"      VARCHAR(20),
+          "PF"          INTEGER,
+          "Yds"         INTEGER,
+          "Ply"         INTEGER,
+          "Y/P"         REAL,
+          "TO"          INTEGER,
+          "FL"          INTEGER,
+          "Tot1stD"    INTEGER,
+          "Cmp"         INTEGER,
+          "PASSAtt"     INTEGER,
+          "PASSYds"     INTEGER,
+          "PASSTD"      INTEGER,
+          "Int"         INTEGER,
+          "NY/A"        REAL,
+          "Pass1stD"    INTEGER,
+          "RUSHAtt"     INTEGER,
+          "RUSHYds"     INTEGER,
+          "RUSHTD"      INTEGER,   
+          "RUSHY/A"     REAL,
+          "RUSH1stD"    INTEGER,
+          "Pen"         INTEGER,
+          "PENYds"      INTEGER,
+          "1stPy"       INTEGER,
+          "#Dr"         INTEGER,
+          "Sc%"         REAL,
+          "TO%"         REAL,
+          "Start"       REAL,
+          "Time"        TIME,
+          "Plays"       REAL,
+          "AVGYds"      REAL,
+          "AVGPts"      REAL,
+          PRIMARY KEY("ttid")
+          );''')
+    # Save (commit) the changes
+    self.conn.commit()
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    self.conn.close()
 
-
+  def addtable(self, tabletoinsert, name):
+    self.conn = sqlite3.connect(self.dn)
+    c = self.conn.cursor()
+    df = tabletoinsert
+    df.to_sql(name, self.conn, if_exists='replace', index=False)
+    # Save (commit) the changes
+    self.conn.commit()
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    self.conn.close()
